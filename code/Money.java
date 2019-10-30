e2c083cd65bd5128cfdf83810c948f86f7db26f2
Adler Melgaço Ferreira <adlermf07@gmail.com>
2019-10-30 19:36:16 -0300

    commit #5

--- code/Money.java
+++ code/Money.java
@@ -1,27 +1,30 @@
-abstract class Money {
-   protected int amount;
-   private String currency; 
+public class Money {
+   	protected int amount;
+   	private String currency; 
 
-   static Money dollar(int amount)  {
-      return new Dollar(amount, "USD");
-   }
-
-   static Money franc(int amount) {
-      return new Franc(amount, "CHF");
-   }
-
-   Money(int amount, String currency) {
-      this.amount = amount;
-      this.currency = currency;
-   }
-
-   public boolean equals(Object object)  {
-      Money money = (Money) object;
-      return amount == money.amount && getClass().equals(money.getClass());
-   }
-   abstract Money times(int multiplier);
+	static Money dollar(int amount)  {
+      	return new Dollar(amount, "USD");
+   	}
+	
+   	static Money franc(int amount) {
+      	return new Franc(amount, "CHF");
+   	}
+    
+   	Money(int amount, String currency) {
+      	this.amount = amount;
+      	this.currency = currency;
+   	}
+	
+   	public boolean equals(Object object) {
+      	Money money = (Money) object;
+      	return amount == money.amount && currency().equals(money.currency());
+   	}
+	
+   	Money times(int multiplier) {
+      	return new Money(amount * multiplier, currency);
+   	}
    
-   String currency() {
-      return currency;
-   }
+   	String currency() {
+      	return currency;
+   	}
 }
