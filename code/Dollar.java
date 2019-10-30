eb09cc080ce91be7daa48f50ade9772703546297
Adler Melgaço Ferreira <adlermf07@gmail.com>
2019-10-30 19:14:18 -0300

    commit #2

--- code/Dollar.java
+++ code/Dollar.java
@@ -1,11 +1,8 @@
-class Dollar {
-   int amount = 10;
-   Dollar(int amount) {}			
-   void times(int multiplier) {}
-}	
-
-public void testMultiplication() {
-   Dollar five = new Dollar(5);
-   five.times(2);
-   assertEquals(10, five.amount);
+public class Dollar extends Money{
+   Dollar(int amount) {
+      this.amount= amount;
+   }
+   void times(int multiplier) {
+      amount= amount * multiplier;
+   }
 }
